597971bf31b7479a629df551db50fb1b869c428f
Egor <113363110+NeverovEgor@users.noreply.github.com>
2025-11-12 21:36:59 +0300

    Update context_processors.py

--- django_app/context_processors.py
+++ django_app/context_processors.py
@@ -1,4 +1,5 @@
 import socket
+import os
 
 def get_hostname():
     return socket.gethostname()
@@ -6,4 +7,5 @@ def get_hostname():
 def pod_info_context(request):
     return {
         'pod_hostname': get_hostname(),
+        'pod_ip': os.getenv('POD_IP', '127.0.0.1'),
     }
